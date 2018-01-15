d0e9257d7265a254ee3e06b479139e8f85bbab0d
Jérémy Barra <jeremy.barra8@gmail.com>
2018-01-15 20:30:22 +0100

    [ADD] Style page des séries

--- series/views.py
+++ series/views.py
@@ -4,10 +4,12 @@ from .models import Serie, Season, Episode
 
 def series(request):
 	context = {'series': Serie.objects.all()}
+	for serie in context['series']:
+		print(serie.image.url)
 	return render(request, 'series/series.html', context)
 
 def seasons(request, serie_id):
-	context = {'seasons': Season.objects.filter(serie=serie_id).values()}
+	context = {'seasons': Season.objects.filter(serie=serie_id)}
 	return render(request, 'series/season.html', context)
 
 def episodes(request, serie_id, season_id):
